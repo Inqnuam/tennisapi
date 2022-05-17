dd926d11933426cc2d7f17f28b6e1b0640c84a09
Inqnuam <vvanderlian@gmail.com>
2022-05-17 10:10:19 +0200

    rollback to aws cp

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,4 +1,3 @@
 rm -rf node_modules logs .env .git
 zip -r express.zip .
-#aws s3 cp express.zip s3://server-release
-aws s3 sync express.zip s3://server-release/express.zip
+aws s3 cp express.zip s3://server-release
