60c864a6a2bcb93b740f02ff835c00c4bc93daec
Inqnuam <vvanderlian@gmail.com>
2022-05-17 21:31:53 +0200

    fixes

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -2,8 +2,4 @@ rm -rf node_modules logs .env .git
 zip -r express.zip .
 aws s3 cp express.zip s3://server-release
 
-aws deploy create-deployment \
-  --application-name TennisCodeDeploy \ 
-  --deployment-config-name TennisCodeDeploy \ 
-  --deployment-group-name TennisCodeDeploy \ 
-  --s3-location bucket=server-release,key=express.zip,bundleType=zip
+aws deploy create-deployment --application-name TennisCodeDeploy --deployment-config-name TennisCodeDeploy --deployment-group-name TennisCodeDeploy --s3-location bucket=server-release,key=express.zip,bundleType=zip
