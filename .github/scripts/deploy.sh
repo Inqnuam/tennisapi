113be715b277d745e74e38c71e1ac3f0be7bb817
Inqnuam <vvanderlian@gmail.com>
2022-05-18 01:01:42 +0200

    zip node_modules as EC2s are in private subnet

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,4 +1,4 @@
-rm -rf node_modules logs .env .git
+rm -rf logs .env .git
 zip -r express.zip .
 aws s3 cp express.zip s3://server-release
 
--- deploy/after_install.sh
+++ deploy/after_install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo chown -R ubuntu /home/ubuntu/tennisapi
 cd /home/ubuntu/tennisapi
-npm i
