e713bcfd9596c3c71b413765d55ae503ae1df71b
Inqnuam <vvanderlian@gmail.com>
2022-05-17 09:58:34 +0200

    use aws sync instead of cp

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,3 +1,4 @@
 rm -rf node_modules logs .env .git
 zip -r express.zip .
-aws s3 cp express.zip s3://server-release
+#aws s3 cp express.zip s3://server-release
+aws s3 sync express.zip s3://server-release/express.zip
