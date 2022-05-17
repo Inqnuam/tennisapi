612a3e5b12daa55f92f7543021ec4aa51e814ae3
Inqnuam <vvanderlian@gmail.com>
2022-05-17 20:13:49 +0200

    trigger code depoyement

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,3 +1,4 @@
 rm -rf node_modules logs .env .git
 zip -r express.zip .
 aws s3 cp express.zip s3://server-release
+aws deploy create-deployment --application-name TennisCodeDeploy --deployment-group-name TennisCodeDeploy --s3Location s3://server-release/express.zip --ignore-application-stop-failures
--- deploy/before_install.sh
+++ deploy/before_install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/tennisapi
 npm run kill
-rm -rf .[a-zA-Z_-]*
-rm -rf ./*
+rm -rf .[a-zA-Z_-]* ./*
 aws s3 cp s3://server-release/env ./.env
