e8a4d1ad8e23527534d1e77c5e9c340d01e7b6f6
Inqnuam <vvanderlian@gmail.com>
2022-05-18 12:30:57 +0200

    rollback to  EC2 installing node_modules

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,4 +1,4 @@
-rm -rf logs .env .git
+rm -rf node_modules logs .env .git
 zip -r express.zip .
 aws s3 cp express.zip s3://server-release
 
--- deploy/after_install.sh
+++ deploy/after_install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo chown -R ubuntu /home/ubuntu/tennisapi
 cd /home/ubuntu/tennisapi
+npm i
